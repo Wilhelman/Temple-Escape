a0c04ad4658359c6feebe647e377792cea3b2ecb
nintervik <vmasogarcia@gmail.com>
2017-12-11 18:38:52 +0100

    Added UISlider class

---
+++ Temple Escape/Motor2D/UISlider.h
@@ -0,0 +1,28 @@
+#ifndef __UISLIDER_H__
+#define __UISLIDER_H__
+
+#include "j1Fonts.h"
+#include "UIElement.h"
+#include "UIButton.h"
+
+class UISlider : public UIElement
+{
+public:
+	SDL_Rect slider_rect;
+	SDL_Rect inner_slider_rect;
+
+private:
+	uint slider_value = 0u;
+	uint max, min = 0u;
+	UIButton* slider_btn = nullptr;
+
+public:
+	UISlider(int x, int y, UI_Type type, SDL_Rect slider_rect, j1Module* callback, UIElement* parent = nullptr);
+
+	void SetSliderButtons(UIButton* slider_btn);
+	void SetSliderValue(uint slider_btn_pos);
+	uint GetSliderValue() const;
+	UIButton* GetSliderButton() const;
+};
+
+#endif //__UISLIDER_H__
