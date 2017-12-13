54524cffa9a2576f68405d3eeff1407cf1695dd2
Guillermo García Subirana <guillermogarsub@gmail.com>
2017-12-13 20:26:41 +0100

    helping p2 :+1:

--- Temple Escape/Motor2D/UICheckbox.cpp
+++ Temple Escape/Motor2D/UICheckbox.cpp
@@ -4,7 +4,7 @@
 #include "j1Audio.h"
 
 
-UICheckBox::UICheckBox(int x, int y, UI_Type type, SDL_Rect cb_tick_normal, SDL_Rect cb_no_tick_normal, SDL_Rect cb_tick_pressed, SDL_Rect cb_no_tick_pressed, SDL_Rect cb_tick_focus, SDL_Rect cb_no_tick_focus, j1Module* callback, UIElement* parent = nullptr) : UIElement(x, y, type, parent)
+UICheckBox::UICheckBox(int x, int y, UI_Type type, SDL_Rect cb_tick_normal, SDL_Rect cb_no_tick_normal, SDL_Rect cb_tick_pressed, SDL_Rect cb_no_tick_pressed, SDL_Rect cb_tick_focus, SDL_Rect cb_no_tick_focus, j1Module* callback, UIElement* parent) : UIElement(x, y, type, parent)
 {
 	this->callback = callback;
 
@@ -19,11 +19,11 @@ UICheckBox::UICheckBox(int x, int y, UI_Type type, SDL_Rect cb_tick_normal, SDL_
 	released_fx = (App->audio->LoadFx(node->attribute("released").as_string()));
 	hover_fx = (App->audio->LoadFx(node->attribute("hover").as_string()));
 
-	this->cb_tick = cb_tick;
-	this->cb_no_tick = cb_no_tick;
+	/*this->cb_tick = cb_tick;
+	this->cb_no_tick = cb_no_tick;*/
 	this->cb_tick_focus = cb_tick_focus;
 	this->cb_no_tick_focus = cb_no_tick_focus;
-	current_rect = cb_tick;
+	//current_rect = cb_tick;
 
 	LOG("UICheckBox created in x:%i, y:%i", x, y);
 }
@@ -40,12 +40,12 @@ UICheckBox::~UICheckBox()
 
 void UICheckBox::UpdateCheckBoxWithSelfRect(SDL_Rect self_rect)
 {
-	if (self_rect.x == btn_pressed.x  && self_rect.y == btn_pressed.y && self_rect.w == btn_pressed.w && self_rect.h == btn_pressed.h)
+	/*if (self_rect.x == btn_pressed.x  && self_rect.y == btn_pressed.y && self_rect.w == btn_pressed.w && self_rect.h == btn_pressed.h)
 		App->audio->PlayFx(pressed_fx);
 	else if (self_rect.x == btn_normal.x  && self_rect.y == btn_normal.y && self_rect.w == btn_normal.w && self_rect.h == btn_normal.h && last_state != STATE_MOUSE_ENTER)
 		App->audio->PlayFx(released_fx);
 	else if (self_rect.x == btn_focused.x  && self_rect.y == btn_focused.y && self_rect.w == btn_focused.w && self_rect.h == btn_focused.h && last_state != STATE_LEFT_MOUSE_RELEASED)
-		App->audio->PlayFx(hover_fx);
+		App->audio->PlayFx(hover_fx);*/
 
 	current_rect = self_rect;
 }
